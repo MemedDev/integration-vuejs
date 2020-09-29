4583734de2c90cafd18bfc717e02fbc085e6b4d9
Gabriela Mendes Pereira Irmão <gabriela.mendes@C02SR23HH3QF.local>
2020-09-29 11:21:24 -0300

    first commit vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
+/* eslint-disable no-undef */
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <span>Memed container</span>
+    <button id="botaoShowPrescricao" style="margin-left: 10px">
+      Gerar prescricao
+    </button>
+    <div class="memed-container" id="memed-cont"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  props: {},
+  data: () => {
+    return {
+      body: document.querySelector("body"),
+    };
+  },
+  methods: {
+    initMemed: function () {
+      let script = document.createElement("script");
+      script.setAttribute("type", "text/javascript");
+      script.setAttribute("data-color", "#1abc9c");
+      script.setAttribute(
+        "data-token",
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.WzMyMDMyLCI2MGY4Y2ZkNWQwYTVjNTc0NGJhYjQyODBlODVhZWY4MSIsIjIwMjAtMDktMjUiLCJzaW5hcHNlLnByZXNjcmljYW8iLCJwYXJ0bmVyLjMuMjc0MzQiXQ.2wUcJ3_ckJ-8uzzFyB8Cy-ECeelGWXDu1wBjMdxdlqs"
+      );
+      script.setAttribute("data-container", "memed-cont");
+      script.src =
+        "https://sandbox.memed.com.br/modulos/plataforma.sinapse-prescricao/build/sinapse-prescricao.min.js";
+      script.onload = function (data) {
+        data.path[3].defaultView.MdSinapsePrescricao.event.add(
+          "core:moduleInit",
+          function moduleInitHandler(module) {
+            console.log(module);
+            if (module.name === "plataforma.prescricao") {
+              document
+                .getElementById("botaoShowPrescricao")
+                .addEventListener("click", function () {
+                  data.path[3].defaultView.MdHub.command
+                    .send("plataforma.prescricao", "setPaciente", {
+                      nome: "José da Silva",
+                      endereco: "Rua da Saúde, 123",
+                      cidade: "São Paulo",
+                      telefone: "11999999999",
+                      peso: 75,
+                      altura: 1.8,
+                      nome_mae: "Nome da mãe",
+                      dificuldade_locomocao: true,
+                      idExterno: 111,
+                    })
+                    .then(function () {
+                      data.path[3].defaultView.MdHub.module.show("plataforma.prescricao");
+                    });
+                });
+            }
+          }
+        );
+      };
+      this.body.appendChild(script);
+    },
+  },
+  mounted() {
+    this.initMemed();
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.memed-container {
+  margin: 0 auto;
+  border: 2px solid black;
+  width: 1000px;
+  height: 710px;
 }
 </style>
